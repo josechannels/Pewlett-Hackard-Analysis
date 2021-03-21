51d0a874554e19d635c5c4b1154c2d1338797b76
josechannels <77767360+josechannels@users.noreply.github.com>
2021-03-20 21:45:20 -0700

    final commit

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,99 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no,dept_no)
+);
+CREATE TABLE Titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no,title,from_date,to_date)
+);
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, to_date DESC;
+
+--count retiring employees by title
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+--mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
+
